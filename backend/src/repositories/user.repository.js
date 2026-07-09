6ebb259f158d9b8c5ebeb2955d6395cb2ced0ccb
Utkarshende <utkarshapd1403@gmail.com>
2026-07-09 20:53:34 +0530

    feat: create user repository

---
+++ backend/src/repositories/user.repository.js
@@ -0,0 +1,28 @@
+import User from "../models/user.model.js";
+
+class UserRepository {
+  async create(userData) {
+    return await User.create(userData);
+  }
+
+  async findByEmail(email) {
+    return await User.findOne({ email }).select("+password");
+  }
+
+  async findById(id) {
+    return await User.findById(id);
+  }
+
+  async updateById(id, updateData) {
+    return await User.findByIdAndUpdate(id, updateData, {
+      new: true,
+      runValidators: true,
+    });
+  }
+
+  async deleteById(id) {
+    return await User.findByIdAndDelete(id);
+  }
+}
+
+export default new UserRepository();
