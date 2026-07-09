d4156594d5fa731bdcedb7e7d880549c86264076
Utkarshende <utkarshapd1403@gmail.com>
2026-07-09 21:28:11 +0530

    feat: add authentication routes and connect to express app

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -5,5 +5,5 @@ dotenv.config();
 export const env = {
   PORT: process.env.PORT || "5000",
   NODE_ENV: process.env.NODE_ENV || "development",
-  MONGODB_URI: process.env.MONGODB_URI as string,
+  MONGODB_URI: process.env.MONGODB_URI,
 };
