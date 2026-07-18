ca977345586a0395e08a90a0ef4327daa759f57c
Utkarshende <utkarshapd1403@gmail.com>
2026-07-18 12:30:19 +0530

    feat: setup frontend routing and axios configuration

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,9 @@
+function Register() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Register Page</h1>
+    </div>
+  );
+}
+
+export default Register;
